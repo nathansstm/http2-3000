d53c3012e7316768997da5422b20332410a1fccb
Author <userland@localhost>
2024-09-22 02:36:58 +0000

    Update

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
 // Paths to your SSL certificate and key using paths from config
 const sslOptions = {
-    key: fs.readFileSync(path.join(__dirname, config.ssl.key)),
-    cert: fs.readFileSync(path.join(__dirname, config.ssl.cert))
+    key: fs.readFileSync(config.ssl.key),
+    cert: fs.readFileSync(config.ssl.cert)
 };
 
 // You can now use sslOptions for your HTTP2 server setup
